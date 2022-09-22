37f2cc60826799541d13e0ecd111aa5e06099904
Majid Shakeel Shawl <majidshakeelshawl@gmail.com>
2022-09-22 10:11:04 +0530

    Added code to fetch details from backend about workouts and also added proxy property to fix CORS error

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,30 @@
+import { useEffect, useState } from "react";
+
 const Home = () => {
+    const [workouts, setWorkouts] = useState(null);
+    useEffect(() => {
+        const fetchWorkouts = () => {
+            fetch('/api/workouts/')
+                .then(response => response.json())
+                .then(data => {
+                    setWorkouts(data)
+                    console.log(data)
+                })
+                .catch(err => console.log(err))
+        }
+        fetchWorkouts();
+    }, []);
+
     return (
         <div className="home">
-            <h1>Home Page</h1>
+            {workouts && workouts.map(workout => {
+                return (
+                    <>
+                        <h2>{workout.title}</h2>
+                        <h3>{workout.load}</h3>
+                    </>
+                );
+            })}
         </div>
     );
 }
