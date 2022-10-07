1b39a7cc2ad2d275bdb1fd5733ec10866d2e5df4
majidshakeelshawl <majidshakeelshawl@gmail.com>
2022-10-08 00:30:46 +0530

    Added bcrypt library to encrypt user passwords and also encrypted user passwords using bcrypt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const Schema = mongoose.Schema;
 
 const userSchema = Schema(
@@ -14,5 +16,18 @@ const userSchema = Schema(
         }
     }
 );
+
+userSchema.statics.signup = async function (email, password) {
+    const emailsExists = await this.findOne({ email });
+    if (emailsExists)
+        throw Error("Email already in use");
+
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
+
+    // new user document
+    return await this.create({ email, password: hash });
+
+}
 
 module.exports = mongoose.model('User', userSchema);
