5a788135112f76623ec8eb56b446e04f965b94d1
majidshakeelshawl <majidshakeelshawl@gmail.com>
2022-10-10 12:51:38 +0530

    Removed node_modules from git repo

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcrypt');
+const validator = require('validator');
 
 const Schema = mongoose.Schema;
 
@@ -18,6 +19,13 @@ const userSchema = Schema(
 );
 
 userSchema.statics.signup = async function (email, password) {
+    //Check for valid email and strong password
+    if (!email || !password)
+        throw Error("Both email and password field must be filled");
+    if (!validator.isEmail(email))
+        throw Error("Enter a valid email");
+    if (!validator.isStrongPassword(password))
+        throw Error("Password is not Strong");
     const emailsExists = await this.findOne({ email });
     if (emailsExists)
         throw Error("Email already in use");
