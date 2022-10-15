ef834951389b097dd435360a860d246d83ccc9f1
majidshakeelshawl <majidshakeelshawl@gmail.com>
2022-10-15 11:51:45 +0530

    Added JWT package and also added functionality to generate token after a user signsup with validity of 3days

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,10 @@
 const mongoose = require('mongoose');
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+    return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
+}
 
 const loginUser = async (req, res) => {
     res.json({ msg: "User login" });
@@ -9,7 +14,9 @@ const signupUser = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.signup(email, password);
-        res.status(200).json({ email, user });
+        //token
+        const token = createToken(user._id);
+        res.status(200).json({ email, token });
     }
     catch (error) {
         res.status(400).json({ err: error.message });
