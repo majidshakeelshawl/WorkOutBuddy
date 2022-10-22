3b74c7eec8b10a659c698acf90d87452599858ae
majidshakeelshawl <majidshakeelshawl@gmail.com>
2022-10-22 15:20:32 +0530

    Added functionality to save auth into local storage and also on the global state

--- frontend/src/hooks/useLogIn.js
+++ frontend/src/hooks/useLogIn.js
@@ -1,5 +1,40 @@
+import { useState } from "react"
 import { useAuthContext } from "./useAuth.context"
 
 export const useLogIn = () => {
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(null);
+    const { dispatch } = useAuthContext();
+
+    const login = async (email, password) => {
+        setIsLoading(true);
+        setError(null);
+
+        const response = await fetch('/api/user/login', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ email, password })
+        });
+        const json = await response.json();
+
+        if (!response.ok) {
+            setIsLoading(false);
+            setError(json.err);
+            console.log(json);
+        }
+        else {
+            //save the token into the local storage
+            localStorage.setItem('user', JSON.stringify(json));
+
+            //update the auth context
+            dispatch({ type: 'LOGIN', payload: json });
+
+            setIsLoading(false);
+        }
+
+    }
+    return { login, error, isLoading };
 
 }
