bbd972aff50f50fefd92bb54683fadda38c30cd8
majidshakeelshawl <majidshakeelshawl@gmail.com>
2022-10-26 13:15:05 +0530

    Added authorization headers for get workouts request

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,22 +2,27 @@ import { useEffect } from "react";
 import WorkoutDetails from "../components/workout-details/WorkoutDetails";
 import { useWorkoutsContext } from '../hooks/useWorkouts.context';
 import WorkoutForm from "../components/workouts-form/WorkoutsForm";
+import { useAuthContext } from '../hooks/useAuth.context';
 
 const Home = () => {
     const { workouts, dispatch } = useWorkoutsContext();
-
+    const { user } = useAuthContext();
     useEffect(() => {
         const fetchWorkouts = () => {
-            fetch('/api/workouts/')
+            fetch('/api/workouts/', {
+                headers: {
+                    'Authorization': `Bearer ${user.token}`,
+                }
+            })
                 .then(response => response.json())
                 .then(data => {
                     //set workouts is for getting all the workouts on the UI not adding new (that's create workout)
                     dispatch({ type: 'SET_WORKOUTS', payload: data });
                 })
-                .catch(err => console.log(err))
         }
-        fetchWorkouts();
-    }, [dispatch]);
+        if (user)
+            fetchWorkouts();
+    }, [dispatch, user]);
 
     return (
         <div className="home">
