003af95d3a9a12f1998e435952e7bd468c2adb4b
majidshakeelshawl <majidshakeelshawl@gmail.com>
2022-10-26 17:02:52 +0530

    secured app routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useAuthContext } from './hooks/useAuth.context';
 import Navbar from './components/navbar/Navbar';
 import Home from './pages/Home';
 import LogIn from './pages/Login';
@@ -6,15 +7,16 @@ import SignUp from './pages/SignUp';
 import './App.css';
 
 function App() {
+  const { user } = useAuthContext();
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
         <div className="pages">
           <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<LogIn />} />
-            <Route path='signup' element={<SignUp />} />
+            <Route path='/' element={user ? <Home /> : <Navigate to='/login' />} />
+            <Route path='/login' element={!user ? <LogIn /> : <Navigate to='/' />} />
+            <Route path='signup' element={!user ? <SignUp /> : <Navigate to='/' />} />
           </Routes>
         </div>
       </BrowserRouter >
