a1fde53e226b3be58948c7d4addbf5d01f2fae50
majidshakeelshawl <majidshakeelshawl@gmail.com>
2022-10-28 16:06:15 +0530

    Added test case for saving a workout in the database and also added functional global variables for user authentication

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -10,9 +10,11 @@ const loginUser = async (req, res) => {
     const { email, password } = req.body;
     try {
         const user = await User.signin(email, password);
+        //id of user
+        const user_id = user._id;
         //token
-        const token = createToken(user._id);
-        res.status(200).json({ email, token });
+        const token = createToken(user_id);
+        res.status(200).json({ email, token, user_id });
     }
     catch (error) {
         res.status(400).json({ err: error.message });
--- backend/routes/workouts.spec.js
+++ backend/routes/workouts.spec.js
@@ -1,24 +1,56 @@
 const request = require('supertest');
 const app = require('../app');
 
+// parameters for logged_in user (not for signed-up)(not testing sign-up yet)
 let user;
+let user_id;
 let token;
 
-describe(('Test GET /workouts'), () => {
-    test('Should respond with 200 status code when user logs in', async () => {
-        //login a user
-        user = await request(app).post('/api/user/login').send(
-            {
-                email: process.env.TEST_EMAIL,
-                password: process.env.TEST_PASSWORD
-            }
-        ).expect(200);
-        token = user.body.token;
+async function currentUser() {
+    const response = await request(app)
+        .post('/api/user/login') //here we need to hit end points because supertest runs our app internally
+        .send({
+            email: process.env.TEST_EMAIL,
+            password: process.env.TEST_PASSWORD
+        });
+    user = response.body;
+    token = user.token;
 
+    // have a look in the backend how the user id is returned after logging in
+    user_id = user.user_id;
+}
+
+describe('Test GET /workouts', () => {
+    beforeAll(async () => {
+        // to set user,token and user_id global variables
+        await currentUser();
     });
+    // test('Should respond with 200 status code when user logs in', () => {
+    //     expect(user);
+    // });
     test('Should respond with 200 status code while getting all the workouts', async () => {
         await request(app).get('/api/workouts')
             .set({ 'Authorization': `Bearer ${token}` })
+            .expect(200);
+    });
+});
+
+describe('Test POST /workout', () => {
+    beforeAll(async () => {
+        // to set user,token and user_id global variables
+        await currentUser();
+    });
+    const workoutData = {
+        user_id: user_id,
+        title: 'Jumps',
+        reps: 12,
+        load: 14,
+    }
+    test('Should respond with 200 status after saving a workout', async () => {
+        const response = await request(app)
+            .post('/api/workouts/')
+            .set({ 'Authorization': `Bearer ${token}` })
+            .send(workoutData)
             .expect(200);
     });
 });
