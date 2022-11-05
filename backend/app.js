3647ad8473242918c6d61eab2a83a05124bdf437
majidshakeelshawl <majidshakeelshawl@gmail.com>
2022-11-05 12:26:47 +0530

    added npm package init to root directory to control both frontend and backend with single scripts and also addded script to serve build version of react app(frontend) from the backend server

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const path = require("path");
 require('dotenv').config();
 
 //routers
@@ -11,11 +12,16 @@ const app = express();
 
 //middleware
 app.use(express.json()); //used for parsing req.body data (alternative for body-parser)
+//for serving public files
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(morgan('dev'));
 
 //routers
 app.use('/api/workouts', workoutRoutes);
 app.use('/api/user', userRoutes);
+app.get("/*", (req, res) => {
+    return res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 //connection to mongodb
 mongoose.connect(process.env.MONGO_URI)
